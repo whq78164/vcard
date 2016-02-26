05a9aa2635898b68199c0412129766239b610ae0
554553400@qq.com <554553400@qq.com>
2016-02-27 01:05:41 +0800

    v1.1

---
+++ frontend/config/main-local.php
@@ -0,0 +1,20 @@
+<?php
+
+$config = [
+    'bootstrap' => ['log'],
+
+    'components' => [
+        'db' => require(__DIR__ . '/db.php'),
+        'i18n' => require(__DIR__ . '/components/i18n.php'),
+        'mailer' => require(__DIR__ . '/components/mailer.php'),
+        //  'as access' => require(__DIR__ . '/components/access.php'),
+    ],
+];
+
+if (YII_ENV_DEV) {
+//if (YII_ENV=='dev') {
+    // configuration adjustments for 'dev' environment
+    require(__DIR__ . '/env.php');
+}
+
+return $config;
